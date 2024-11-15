794525952501a9b6d9d204685f5cfe9248277d8b
volker-baecker <volker.baecker@mri.cnrs.fr>
2024-11-15 18:14:49 +0100

    adding the columns is almost working

--- convert_table.groovy
+++ convert_table.groovy
@@ -3,21 +3,62 @@ import java.awt.Frame
     
 STAININGS = ["CD44v6", "Ki67"]
 
-fileDialog = new FileDialog((Frame)null, "Please select the input file!", FileDialog.LOAD)
-fileDialog.show()
-folder = fileDialog.getDirectory()
-filename = fileDialog.getFile()
-def file = new File(folder, filename)
-data = file.readLines()*.split('\t')
-combinations = calculateCombinations(STAININGS)
-columns = data[0]
+def file = getFileFromUser()
+def data = file.readLines()*.tokenize('\t')
+
+def columns = []
+columns.addAll(0, data[0])
 data = data.tail()
+combinations = calculateCombinations(STAININGS)
 
 print(combinations)
+print("columns")
+print(columns.getClass())
+print(data.size())
+
+print("mark 0")
+
+for (combination in combinations) {
+    print("mark 0.1")
+    print(combination.join("_").getClass())
+    columns.add("Total " + combination.join("_"))
+    print("mark 0.2")
+}
+
 print(columns)
+print("mark 1")
+for (int rowIndex = 0; rowIndex < data.size(); rowIndex++) {
+    print("mark 1.1")
+    for (combination in combinations) {
+        row = data[rowIndex]
+        print("mark 1.1.1")
+        sum = 0
+        for (int columnIndex = 0; columnIndex < row.size(); columnIndex++) {
+            print("mark 1.1.1.1")
+            if (containsAll(columns[columnIndex], combination)) {
+                print("mark 1.1.1.1.1")
+                print(data[rowIndex][columnIndex])
+                print(data[rowIndex][columnIndex].getClass())
+                value = data[rowIndex][columnIndex] ? data[rowIndex][columnIndex].toInteger() : 0
+                sum = sum + value
+            }
+        }
+        print("mark 1.1.2")
+        for (int columnIndex = 0; columnIndex < columns.size(); columnIndex++) {
+            if (columns[columnIndex].contains("Total " + combination.join("_"))) {
+                print("mark 1.1.2.1")
+                data[rowIndex][columnIndex] = sum
+            }
+        }
+    }
+}
+
 print(data)
 
-for i in 
+/**
+ * Answer all single occurences and combinations of elements except 
+ * for the combination containing all elements.
+ */
 public List<List<String>> calculateCombinations(List<String> elements) {
     List<List<String>> combinationList = new ArrayList<List<String>>();
     for ( long i = 1; i < Math.pow(2, elements.size()); i++ ) {
@@ -29,5 +70,25 @@ public List<List<String>> calculateCombinations(List<String> elements) {
         }
         combinationList.add(list);
     }
-    return combinationList;
+    return combinationList[0..combinationList.size()-2];
+}
+
+
+public boolean containsAll(String text, List<String> items) {
+    result = true
+    for (item in items) {
+        result = result && text.contains(item)
+    }
+    return result
+}
+
+
+
+public File getFileFromUser() {
+    fileDialog = new FileDialog((Frame)null, "Please select the input file!", FileDialog.LOAD)
+    fileDialog.show()
+    folder = fileDialog.getDirectory()
+    filename = fileDialog.getFile()
+    file = new File(folder, filename)
+    return file
 }
