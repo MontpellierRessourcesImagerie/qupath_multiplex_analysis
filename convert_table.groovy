64b90765e2d4827eeef24bbd911de22a2e82aae1
volker-baecker <volker.baecker@mri.cnrs.fr>
2024-11-18 16:24:31 +0100

    it's working now

--- convert_table.groovy
+++ convert_table.groovy
@@ -1,59 +1,53 @@
 import java.awt.FileDialog
 import java.awt.Frame
-    
+
 STAININGS = ["CD44v6", "Ki67"]
 
 def file = getFileFromUser()
-def data = file.readLines()*.tokenize('\t')
-
+def data = file.readLines()*.split('\t')
+def outFile = getOutFile(file)
 def columns = []
+
 columns.addAll(0, data[0])
-data = data.tail()
-combinations = calculateCombinations(STAININGS)
+dataOriginal = data.tail()
+data = []
 
-print(combinations)
-print("columns")
-print(columns.getClass())
-print(data.size())
-
-print("mark 0")
-
-for (combination in combinations) {
-    print("mark 0.1")
-    print(combination.join("_").getClass())
-    columns.add("Total " + combination.join("_"))
-    print("mark 0.2")
+for (rowIndex=0; rowIndex<dataOriginal.size(); rowIndex++) {
+   row = []
+   for(columnIndex=0; columnIndex<dataOriginal[0].size(); columnIndex++) {
+       row.add(dataOriginal[rowIndex][columnIndex])
+   }
+   data.add(row)
 }
 
-print(columns)
-print("mark 1")
+combinations = calculateCombinations(STAININGS)
+
+for (combination in combinations) {
+    columns.add("Total " + combination.join("_"))
+}
+
 for (int rowIndex = 0; rowIndex < data.size(); rowIndex++) {
-    print("mark 1.1")
     for (combination in combinations) {
         row = data[rowIndex]
-        print("mark 1.1.1")
         sum = 0
-        for (int columnIndex = 0; columnIndex < row.size(); columnIndex++) {
-            print("mark 1.1.1.1")
-            if (containsAll(columns[columnIndex], combination)) {
-                print("mark 1.1.1.1.1")
-                print(data[rowIndex][columnIndex])
-                print(data[rowIndex][columnIndex].getClass())
-                value = data[rowIndex][columnIndex] ? data[rowIndex][columnIndex].toInteger() : 0
+        for (int columnIndex = 0; columnIndex < columns.size(); columnIndex++) {
+            if (containsAll(columns[columnIndex], combination + ['Num'])) {
+                value = data[rowIndex][columnIndex] ? data[rowIndex][columnIndex].toInteger() : 0    
                 sum = sum + value
             }
         }
-        print("mark 1.1.2")
         for (int columnIndex = 0; columnIndex < columns.size(); columnIndex++) {
             if (columns[columnIndex].contains("Total " + combination.join("_"))) {
-                print("mark 1.1.2.1")
                 data[rowIndex][columnIndex] = sum
             }
         }
     }
 }
 
-print(data)
+header = columns.join('\t') + "\n"
+outFile.text = header + data*.join('\t').join(System.lineSeparator())
+
+
 
 /**
  * Answer all single occurences and combinations of elements except 
@@ -91,4 +85,16 @@ public File getFileFromUser() {
     filename = fileDialog.getFile()
     file = new File(folder, filename)
     return file
+}
+
+
+public File getOutFile(File file) {
+    path = file.path
+    String fileWithoutExt = file.name.take(file.name.lastIndexOf('.'))
+    nameExtension = "_mod"
+    ext = ".tsv"
+    directory = file.getParentFile()
+    filename = fileWithoutExt + nameExtension + ext
+    outFile = new File(directory, filename)
+    return outFile
 }
